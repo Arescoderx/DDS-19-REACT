771be6733f386dfbbe5f4f9a2e542d734a0b1608
Matheus <matheus.schmidt@aluno.senai.br>
2025-03-17 20:55:22 -0300

    Tirando O

--- inicio/src/components/TemplateExpressions.jsx
+++ inicio/src/components/TemplateExpressions.jsx
@@ -13,7 +13,7 @@ const TemplateExpressions = () => {
   return (
     <div>
       <h1>O nome do Jogador é {nome}</h1>
-      <p>O nome do aluno é {aluno.nome}, faz o curso de {aluno.curso} na turma {aluno.turma} e tem {aluno.idade} anos</p>
+      <p>nome do aluno é {aluno.nome}, faz o curso de {aluno.curso} na turma {aluno.turma} e tem {aluno.idade} anos</p>
       <p>Seu nome tem {aluno.nome.length} letras</p>
       <p>Seu nome em maiúsculo é {aluno.nome.toUpperCase()}</p>
       <p>Seu nome em minúsculo é {aluno.nome.toLowerCase()}</p>
